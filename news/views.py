28298c923cde6f198223e5024d518616f526dcb4
senkoffyura <142835691+senkoffyura@users.noreply.github.com>
2023-10-26 23:13:10 +0500

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-time_creates'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'headings'
+
+
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        return context
+
+class PostDetail(DetailView):
+
+    model = Post
+    template_name = 'text_news.html'
+
+    context_object_name = 'heading'
+
+
+
